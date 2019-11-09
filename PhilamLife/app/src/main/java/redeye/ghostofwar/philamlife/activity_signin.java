410eb71f35f2c039d07c7a7a4db006ffa075a2a5
zheuswalker <thndrwrth@gmail.com>
2019-11-09 10:40:27 +0800

    intial setup

---
+++ PhilamLife/app/src/main/java/redeye/ghostofwar/philamlife/activity_signin.java
@@ -0,0 +1,13 @@
+package redeye.ghostofwar.philamlife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_signin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
